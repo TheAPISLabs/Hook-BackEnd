043610801ec0b0211c7886e93410bea63e61c7f2
Admas <liulplss@gmail.com>
2022-07-25 17:58:09 +0800

    token optimize

--- apis/src/main/java/com/yike/apis/utils/tokenView/TokenViewApi.java
+++ apis/src/main/java/com/yike/apis/utils/tokenView/TokenViewApi.java
@@ -1,14 +1,14 @@
 package com.yike.apis.utils.tokenView;
 
 public class TokenViewApi {
-    final static String tokenbalance = "https://eth.tokenview.com/api/eth/address/tokenbalance/%s";
-    final static String tokentrans = "https://eth.tokenview.com/api/eth/address/tokentrans/%s/%s/%s/%s";
-    final static String token = "https://eth.tokenview.com/v12api/gettokens/eth/token";
-    final static String maincoinexchange = "https://eth.tokenview.com/api/coin/maincoinexchange";
-    final static String normal = "https://eth.tokenview.com/api/eth/address/normal/%s/%s/%s";
-    final static String contract = "https://eth.tokenview.com/api/eth/contract/creator/%s";
-    final static String tokenEth = "https://eth.tokenview.com/api/token/eth/%s";
-    final static String tokenTokentrans = "https://eth.tokenview.com/api/eth/token/tokentrans/%s/%s/%s";
-    final static String txeth = "https://eth.tokenview.com/api/tx/eth/%s";
-    final static String address = "https://eth.tokenview.com/api/eth/address/%s";
+    final static String tokenbalance = "https://eth.tokenview.io/api/eth/address/tokenbalance/%s";
+    final static String tokentrans = "https://eth.tokenview.io/api/eth/address/tokentrans/%s/%s/%s/%s";
+    final static String token = "https://eth.tokenview.io/v12api/gettokens/eth/token";
+    final static String maincoinexchange = "https://eth.tokenview.io/api/coin/maincoinexchange";
+    final static String normal = "https://eth.tokenview.io/api/eth/address/normal/%s/%s/%s";
+    final static String contract = "https://eth.tokenview.io/api/eth/contract/creator/%s";
+    final static String tokenEth = "https://eth.tokenview.io/api/token/eth/%s";
+    final static String tokenTokentrans = "https://eth.tokenview.io/api/eth/token/tokentrans/%s/%s/%s";
+    final static String txeth = "https://eth.tokenview.io/api/tx/eth/%s";
+    final static String address = "https://eth.tokenview.io/api/eth/address/%s";
 }
